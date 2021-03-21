4b629c4de3217d2e48ad2a0695987346804d7fe9
Alexander Koskovich <zvnexus@outlook.com>
2021-03-20 19:08:05 -0700

    juice: Import vendor WiFi & framework overlays.
     * From LA.QSSI.11.0.r1-10700-qssi.0.
    
    Change-Id: I3d426d23fe8aa5d4c1c535b772fc426125fae715

--- device.mk
+++ device.mk
@@ -235,6 +235,11 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3.vendor
 
+# Overlays
+PRODUCT_PACKAGES += \
+    FrameworksResTarget \
+    WifiResTarget
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
