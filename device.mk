f142fa1aa7ea1bc17c066c4d35a4fc6fdbfb251a
Gagan Malvi <malvigagan@gmail.com>
2021-11-01 05:25:39 +0100

    juice: Import WCNSS configuration from LA.UM.9.15.2.r1-05800-KAMORTA.QSSI12.0.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -195,3 +195,7 @@ $(call inherit-product, vendor/xiaomi/juice/juice-vendor.mk)
 # Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
+# WLAN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
