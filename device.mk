391539ebb21e22dbd4d60970a2553b222b4f2e30
Albert I <kras@raphielgang.org>
2021-12-23 12:10:12 +0530

    juice: Remove touchfeature from sensor HALs loading list
    * Move sensors/hals.conf to device tree to make it possible
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- device.mk
+++ device.mk
@@ -327,6 +327,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
