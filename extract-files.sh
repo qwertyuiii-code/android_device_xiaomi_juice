b6ce5e040df03a2dc9e2b5d7bbcab034bb3091c5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-21 16:14:42 +0400

    juice: proprietary: Pin modified vendor.qti.hardware.camera.postproc@1.0-service-impl.so
    The modified library NOPs out the instruction to change binder
    threadpool, allowing the library to load and camera to work on S.
    
    Command used:
    $ SigScan -p "13 0A 00 94" -P "1F 20 03 D5" -f vendor.qti.hardware.camera.postproc@1.0-service-impl.so
    
    Change-Id: I1ef0ed29e4a2eb820826e5d7df1fa53c9e3df584
    Signed-off-by: boedhack99 <boedhack@gmail.com>
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
+<<<<<<< HEAD
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017-2020 The LineageOS Project
 # Copyright (C) 2021 Paranoid Android
+=======
+# Copyright (C) 2022 The LineageOS Project
+>>>>>>> 0b28480 (juice: proprietary: Pin modified vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -16,7 +20,7 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
+ANDROID_ROOT="${MY_DIR}"/../../..
 
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
@@ -27,9 +31,6 @@ source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction.
 CLEAN_VENDOR=true
-
-KANG=
-SECTION=
 
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
@@ -54,9 +55,18 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            "${SIGSCAN}" -p "13 0A 00 94" -P "1F 20 03 D5" -f "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper.
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
+        "${KANG}" --section "${SECTION}"
 
 "${MY_DIR}/setup-makefiles.sh"
