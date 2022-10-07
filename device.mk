172fabef8939249fd16cd07161ebbbd3ad2f4e74
johnmart19 <johnivan19999@gmail.com>
2022-10-08 02:13:09 +0300

    juice: Build legacy android.frameworks.sensorservice@1.0
    [   19.866353] init: Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 637 (/system/bin/hwservicemanager)

--- device.mk
+++ device.mk
@@ -335,7 +335,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
 
